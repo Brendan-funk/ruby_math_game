230394c7c87f6de7b8ca01439c0d6ded1e6c1c94
Brendan-funk <brendanzfunk@gmail.com>
2022-04-04 19:30:16 +0000

    adds question class

--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
 require './player.rb'
+require './question.rb'
 p1 = Player.new(1)
 p2 = Player.new(2)
+q1 = Question.new
 puts p1.lives
 p1.lives = 2
 puts p2.lives
 puts p1.lives
+puts q1.string
+puts q1.answer
